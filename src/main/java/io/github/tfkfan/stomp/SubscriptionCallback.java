fb21f50b7558df79373649eef40f9c647437feb7
tfkfan <sylar131@live.ru>
2025-07-17 11:35:37 +0300

    Refactored SubscriptionCallback

--- src/main/java/io/github/tfkfan/stomp/SubscriptionCallback.java
+++ src/main/java/io/github/tfkfan/stomp/SubscriptionCallback.java
@@ -2,6 +2,7 @@ package io.github.tfkfan.stomp;
 
 import io.vertx.ext.stomp.ServerFrame;
 
+@FunctionalInterface
 public interface SubscriptionCallback {
     void onMessage(ServerFrame serverFrame);
 }
